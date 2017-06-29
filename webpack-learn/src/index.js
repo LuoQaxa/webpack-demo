7e0e30fdee38048ebd46ed11c03fcf0404627d46
luoqian <szuluoqian@163.com>
2017-06-29 15:26:12 +0800

    getting started

---
+++ webpack-learn/src/index.js
@@ -0,0 +1,10 @@
+// index.js
+
+import _ from 'lodash';
+function component() {
+	var element = document.createElement('div');
+	element.innerHTML = _.join(['Hello','webpack'],' ');
+	return element;
+}
+document.body.appendChild(component());
+
