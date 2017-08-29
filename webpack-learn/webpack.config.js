f893d8566bb3dc239cf8fd91895cc9982fb72191
luoqian <szuluoqian@163.com>
2017-08-29 17:54:53 +0800

    第三方库打包、生成html文件插件的使用

--- webpack-learn/webpack.config.js
+++ webpack-learn/webpack.config.js
@@ -1,10 +1,26 @@
 // webpack.config.js
 var path = require('path');
-
+var pkg = require('./package.json');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+  	app: path.resolve(__dirname,'src/index.js'),
+  	// index:'./src/index.js',
+  	vendor: Object.keys(pkg.dependencies)
+  },
+  
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+
+  plugins:[
+    new HtmlWebpackPlugin({
+    	template: __dirname + '/src/index.tmpl.html'
+    }),
+  	new webpack.optimize.CommonsChunkPlugin({
+  		names: ['vendor'],
+  	})
+  ]
 };
