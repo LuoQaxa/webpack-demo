f5b91795d3525ba002d69fb7132b0c3de468e1db
luoqian <szuluoqian@163.com>
2019-03-06 09:28:27 -0800

    feat webpack-dev-server htmlwebpackplugin

--- README.md
+++ README.md
@@ -4,3 +4,22 @@ webpack4.0
 ## 基础配置
 1. 依赖： `webpack webpack-cli`
 2. 支持零配置打包
+
+## webpack-dev-server
+内部是用express搭建的静态服务器，每次打包的时候并没有真正的打包成文件，而是放在内存中，使得开发速度很快
+默认以当前目录做为静态目录
+1. 常用配置
+```js
+devServer: {
+  port: 3000,
+  open: true,
+  // 是在哪个目录下启动服务,一般是在打包后的目录
+  contentBase: './dist'
+}
+```
+
+## 插件Plugin
+1. HtmlWebpackPlugin： 解决了不需要在打包后的文件中去手动建立一个`index.html`,可以指定任意路径的html作为模版，并且自动将打包后的js文件添加到html中。
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[hash:8].js',
     path: path.resolve(__dirname, 'dist') //必须是一个绝对路径
-  }
+  },
+  devServer: { //开发服务器的配置
+    port: 3000,
+    progress: true,
+    contentBase: './dist',
+    open: true
+  },
+  plugins: [ // 数组
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      // 对html的压缩操作
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+      },
+      hash: true
+    })
+  ]
 }
